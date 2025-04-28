80a84661ebe7a1eaac3bb0ce60fafe1c8af671e5
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-04-28 21:39:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:internet/screens/home/view/post_page.dart';
+
+void main() async{
+  runApp(new MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PostPage(),
+    );
+  }
+}
---
+++ package/user_repository/libs/src/firebase_user_repo.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'entities/entities.dart';
+import 'models/user.dart';
+import 'user_repo.dart';
+
+class FirebaseUserRepo implements UserRepository{
+
+  final FirebaseAuth _firebaseAuth;
+  final userCollection = FirebaseFirestore.instance.collection('users');
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<void> logOut() async{
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  Future<void> setUserData(MyUser myUser, String password) {
+    try{
+      UserCredential user = await _firebaseAuth.signInWithEmailAndPassword(
+          email: myUser.email,
+          password: password
+      );
+      myUser.userId = user.user!.uid;
+      return myUser;
+    } catch(e) {
+      log(e.toString( ));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signIn(String email, String password) async{
+    try{
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+    } catch(e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async{
+    try{
+     UserCredential user = await _firebaseAuth.signInWithEmailAndPassword(
+          email: myUser.email,
+          password: password
+      );
+      myUser.userId = user.user!.uid;
+      return myUser;
+    } catch(e) {
+      log(e.toString( ));
+      rethrow;
+    }
+  }
+
+  @override
+  Stream<MyUser?> get user {
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async*{
+      if(firebaseUser == null){
+        yield MyUser.empty;
+      }else {
+        yield await userCollection
+            .doc(firebaseUser.uid)
+            .get()
+            .then((value) => MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!)) );
+      }
+    });
+  }
+
+
+}
